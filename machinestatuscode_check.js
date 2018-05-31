329a0d42db88d2feca854671ff3ab09a516f64ae
nnk95 <39042433+nnk95@users.noreply.github.com>
2018-05-31 11:16:31 +0800

    v1
    Set a machine status code and then get it back to see what the code reflects, along with user waits.

---
+++ machinestatuscode_check.js
@@ -0,0 +1,26 @@
+var count = 0
+
+function script1(count) {
+
+    var nextcfm = confirm("next?")
+    if (nextcfm == true) {
+    var pcode = printer.udw("ds2.set 65541 ")
+    var everything = shell("udws 'ds2.set 65541 '" + count)
+        console.warn(everything)
+    var stat = printer.udw("ds2.get 65541")
+        script3(count)
+    }
+    else
+        script2()
+}
+
+function script2(){
+    console.log("end")
+}
+
+function script3(count) {
+    var count = count + 1
+    script1(count)
+}
+
+script1(count)
