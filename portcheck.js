3b83ccc047214ccb1343c0df6acab933b4dfaa84
nnk95 <39042433+nnk95@users.noreply.github.com>
2018-06-20 16:42:46 +0800

    v0.1
    A testing version of the port checker and switcher script

---
+++ portcheck.js
@@ -0,0 +1,32 @@
+// SIFT2: port changer / checker script
+// version 0.1 - 20.06.2018 (Contact: Jason Teo)
+
+printer.promptAll()
+var wrongport = 0
+
+printer.onShell("/bin/sh: udws: not found", "console.log('onshell entering backontrack prog'); backontrack(wrongport = 1)")
+
+function backontrack(wrongport) {
+    console.log("backontrack prog starting")
+    while (wrongport == 1) {
+        console.log("while loop active")
+    printer.promptAll()
+    sift.mSleep(10)
+    printer.setConnection("X:")
+        console.log("Port resetting to X")
+    var wrongport = 0
+    printer.clearOneWatcher("/bin/sh: udws: not found")
+    }
+    console.log("exiting backontrack prog")
+}
+
+function mainfunct() {
+        console.log("main function start")
+    printer.udw("port.probe_test")
+        var v001 = printer.udw("ds2.get 65541")
+        printer.udw("smgr_init.auto_reboot_on_assert 10")
+    console.log(v001)
+    console.log("main function end")
+}
+
+mainfunct()
